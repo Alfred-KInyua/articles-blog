8df5f66168d2032119b69ec76ea3ee622b346e03
Kinyua <alfkinyua@gmail.com>
2022-11-27 12:17:42 +0300

    Add model, views, controllers and routes,

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pages_home', to: 'pages#home'
+  get 'pages_about', to: 'pages#about'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
